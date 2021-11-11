622c9380880a8a42cd8668f3d45aefb4d8ea158e
wilasinee1564 <88995199+wilasinee1564@users.noreply.github.com>
2021-11-11 13:28:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,10 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=msg)
+        if (msg == "สวัสดี") :
+            replyObj = TextSendMessage(text="ดีด้วย")
+        else :
+            replyObj = TextSendMessage(text=msg)
         line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
