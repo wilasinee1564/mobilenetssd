9c2210695e675dcdfb51ad17033685e59ecc8430
wilasinee1564 <88995199+wilasinee1564@users.noreply.github.com>
2021-11-18 13:26:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def event_handle(event):
             replyObj = TextSendMessage(text="ซื้อสิ")
         else :
             replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+            line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
