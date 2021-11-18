278c801a4d767381baacdf743fe2aae7d12e8981
wilasinee1564 <88995199+wilasinee1564@users.noreply.github.com>
2021-11-18 16:48:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -180,8 +180,8 @@ def event_handle(event):
             '''
             replyObj = TextSendMessage(text=json_headers)
             line_bot_api.reply_message(rtoken, replyObj)
-        elif msgType == "image":
-            try:
+    elif msgType == "image":
+        try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
             i = Image.open(BytesIO(message_content.content))
             filename = event['message']['id'] + '.jpg'
